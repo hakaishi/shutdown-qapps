42cc35f52202dee6e8c7fb91e580f6ecdde1d50a
hakaishi <hakaishi@web.de>
2017-01-14 13:27:17 +0900

    updated copy right date and version up

--- qshutdown/src/about.cpp
+++ qshutdown/src/about.cpp
@@ -1,5 +1,5 @@
 /* qshutdown, a program to shutdown/reboot/suspend/hibernate the system
- * Copyright (C) 2010-2016 Christian Metscher <hakaishi@web.de>
+ * Copyright (C) 2010-2017 Christian Metscher <hakaishi@web.de>
 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -34,7 +34,7 @@ About::About(QWidget *parent): QDialog(parent){
                                "each day of the week.</p>"));
      textBrowser->setText(tr("<p >qshutdown, a time and countdown based shutdown tool</p>"
                              "<p >Author: Christian Metscher &lt;hakaishi@web.de&gt;</p>"
-                             "<p >Copyright &copy; 2010-2016, by Christian Metscher</p>"
+                             "<p >Copyright &copy; 2010-2017, by Christian Metscher</p>"
                              "<p ><a href=\"https://launchpad.net/~hakaishi\">"
                              "<span style=\" text-decoration: underline; "
                              "color:#0000ff;\">https://launchpad.net/~hakaishi</span></a></p>"
